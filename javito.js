358155d9340b91abe2e4dcc0850c9b2738d07454
Ampo812 <49953880+Ampo812@users.noreply.github.com>
2023-10-17 09:43:18 +0200

    Add files via upload

---
+++ javito.js
@@ -0,0 +1,27 @@
+function doboz()
+{
+    CSSStyleSheet( "javito.css")
+    {
+        IDBDatabase("container");
+    }
+
+}
+
+function egy()
+{
+    CSSStyleSheet("javito.css")
+    {
+        id
+    }
+
+}
+
+function ketto()
+{
+
+}
+
+function harom()
+{
+
+}
